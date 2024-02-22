e46d1e91dde2ea0c96d9aa40c885e14c4efe8513
AntoineChartier1 <1363491@cstj.qc.ca>
2024-02-21 22:11:55 -0500

    test enleve source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `chien`,
-        path: `${__dirname}/chien`,
-      }
-    },
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
   ],
